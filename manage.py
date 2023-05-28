429b6ff540275ad19bffa2e2b844a92a53ac045f
corpobear <martin.kasala@outlook.com>
2023-05-28 19:48:37 +0200

    introduced test and cov commands

--- manage.py
+++ manage.py
@@ -1,14 +1,38 @@
 from app import create_app
 from flask.cli import FlaskGroup
+import coverage
+import unittest
 
 
 app = create_app()
 cli = FlaskGroup(create_app=create_app)
+COV = coverage.coverage(omit=['app/tests/*'], include='app/*')
+COV.start()
+
 
 @cli.command()
 def test():
-    # run our tests here
-   pass
+    tests = unittest.TestLoader().discover('app/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    
+    if result.wasSuccessful():
+        
+        return 0
+    return 1
+
+
+@cli.command()
+def cov():
+    tests = unittest.TestLoader().discover('app/tests', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    
+    if result.wasSuccessful():
+        COV.stop()
+        print('Coverage summary:')
+        COV.report(show_missing=True)
+        
+        return 0
+    return 1
 
 
 if __name__ == '__main__':
